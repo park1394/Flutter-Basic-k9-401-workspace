3aea2f074a1205a925003dbbbc41ee2ce929c670
lsy3709 <lsy3709@naver.com>
2026-03-30 11:49:11 +0900

    3교시, 클래스 샘플코드11, 6_generic

--- lib/ch2_2/5_abstract_class.dart
+++ lib/ch2_2/5_abstract_class.dart
@@ -19,11 +19,12 @@ class GirlGroup implements Idol {
       this.name,
       this.membersCount,
       );
-
+  @override
   void sayName() {
     print('저는 여자 아이돌 ${this.name}입니다.');
   }
 
+  @override
   void sayMembersCount() {
     print('${this.name} 멤버는 ${this.membersCount}명입니다.');
   }
